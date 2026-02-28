094dc5d79c37c1a5afd865bd7ae58415b11b2349
ZeEvoker2Vex <zeevoker2vex.pro.fr@gmail.com>
2026-02-28 13:04:38 +0100

    feat: player controller and game pause logic
    Player controller: basic WASD movements, sprint, jump, look around

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class GameLogic : MonoBehaviour
+{
+    public static bool Paused { get; set; } = false;
+
+
+    [SerializeField] private Transform pauseUi;
+
+    private PlayerInputActions playerInputActions;
+
+    void Awake()
+    {
+        playerInputActions = new PlayerInputActions();
+
+        pauseUi.gameObject.SetActive(false);
+
+        playerInputActions.Global.Enable();
+        playerInputActions.Global.Pause.performed += Pause;
+    }
+
+    void Pause(InputAction.CallbackContext context)
+    {
+        GameLogic.Paused = !GameLogic.Paused;
+        if (GameLogic.Paused)
+        {
+            pauseUi.gameObject.SetActive(true);
+            playerInputActions.Player.Disable();
+            playerInputActions.UI.Disable();
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            Time.timeScale = 0.0f;
+        }
+        else
+        {
+            pauseUi.gameObject.SetActive(false);
+            playerInputActions.Player.Enable();
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    void OnDestroy()
+    {
+        playerInputActions.Global.Disable();
+    }
+}
